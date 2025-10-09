1d0592cfda7284761bede70c3468356e3a1bd460
Mateoamaya <mateoamaya.2007@gmail.com>
2025-10-08 21:56:33 -0500

    pascal triangles

---
+++ entrega2/pascalstriangle.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class Solution {
+    public:
+        vector<vector<int>> generate(int filas) {
+            vector<vector<int>> triangle(filas);          
+            for (int i = 0; i < filas; i++) {
+                triangle[i].resize(i + 1);                    
+                triangle[i][0] = triangle[i][i] = 1;          
+                for (int j = 1; j < i; j++) {                 
+                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+                }
+            }
+            return triangle;
+        }
+};
+int main() {
+    Solution sol;
+    int filas;
+    cout << "Ingrese el número de filas para el Triángulo de Pascal: ";
+    cin >> filas;
+    vector<vector<int>> triangle = sol.generate(filas);
+    cout << "Triángulo de Pascal con " << filas << " filas:" << endl;
+    string c = "";
+    for (int i=0; i<filas; i++) {
+        
+        for (int j=0 ; j < triangle[i].size() ; j++) {
+            c= c + to_string(triangle[i][j]) + " ";
+        }
+        c=c +  "\n";
+    }
+    cout << c;
+    return 0;
+} 
