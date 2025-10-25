af642d5e0065cf225c50559df991a473d43b0af9
Mateoamaya <mateoamaya.2007@gmail.com>
2025-10-24 19:03:02 -0500

    compilacion

---
+++ entrega33/alquivehicles/build/CMakeFiles/SistemaAlquiler.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mateo_amaya/alse/entregas/entrega33/alquivehicles/src/Auto.cpp" "/home/mateo_amaya/alse/entregas/entrega33/alquivehicles/build/CMakeFiles/SistemaAlquiler.dir/src/Auto.cpp.o"
+  "/home/mateo_amaya/alse/entregas/entrega33/alquivehicles/src/Bicicleta.cpp" "/home/mateo_amaya/alse/entregas/entrega33/alquivehicles/build/CMakeFiles/SistemaAlquiler.dir/src/Bicicleta.cpp.o"
+  "/home/mateo_amaya/alse/entregas/entrega33/alquivehicles/src/SistemaAlquiler.cpp" "/home/mateo_amaya/alse/entregas/entrega33/alquivehicles/build/CMakeFiles/SistemaAlquiler.dir/src/SistemaAlquiler.cpp.o"
+  "/home/mateo_amaya/alse/entregas/entrega33/alquivehicles/src/Vehiculo.cpp" "/home/mateo_amaya/alse/entregas/entrega33/alquivehicles/build/CMakeFiles/SistemaAlquiler.dir/src/Vehiculo.cpp.o"
+  "/home/mateo_amaya/alse/entregas/entrega33/alquivehicles/src/main.cpp" "/home/mateo_amaya/alse/entregas/entrega33/alquivehicles/build/CMakeFiles/SistemaAlquiler.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ entrega33/alquivehicles/build/CMakeFiles/SistemaAlquiler.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SistemaAlquiler.dir/src/Auto.cpp.o"
+  "CMakeFiles/SistemaAlquiler.dir/src/Bicicleta.cpp.o"
+  "CMakeFiles/SistemaAlquiler.dir/src/SistemaAlquiler.cpp.o"
+  "CMakeFiles/SistemaAlquiler.dir/src/Vehiculo.cpp.o"
+  "CMakeFiles/SistemaAlquiler.dir/src/main.cpp.o"
+  "SistemaAlquiler"
+  "SistemaAlquiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SistemaAlquiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
